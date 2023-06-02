738c2740d8b9dd8feb97c1903f1530cbd6e12e36
Enrique Pinedo <epinedo31@outlook.com>
2023-06-01 19:46:24 -0500

    06012023

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # 4-Code-Quiz
-Challenge 4 
+## Description
+
+The purpose of this Challenge-4 was to create a timed quiz with a Javascript theme. There is a public Javascript quiz in w3Schools; and I was able to grab sample questions for my quiz from there. Moving on, my quiz is scored by time rather than by a grade due to the nature of the website. The graphics of this quiz is very plain at best, but the main focus was for the site to be operational. Thanks to this challenge I was able to get a better understanding of Arrays, buttons, and how to get/call elements, classes, and id's.
+
+## Installation
+In order to install this repository you will need to navigate into my github "https://github.com/epinedo3/4-Code-Quiz". Once you are there, look for the green code button. Copy the SSH link that is provided. Enter your local gitbash/terminal. Once you are in your desired destination folder. Type in the git clone command and paste the provided link. Execute and Enjoy.
+
+## Usage
+To find my github, click [here](https://github.com/epinedo3/4-Code-Quiz).
+
+To find my deployed website, click [here](https://epinedo3.github.io/4-Code-Quiz/).
+
+## Credits
+
+w3Schools: https://www.w3schools.com/js/js_quiz.asp
+
+## License
+
+Please refer to the LICENSE in the repo.
--- script/script.js
+++ script/script.js
@@ -1,31 +1,47 @@
 var startBtn = document.getElementById('start');
 var quizScreen = document.getElementById('quiz-screen')
 var timeEl = document.querySelector(".timer");
-const backButton = document.getElementById('backButton');
-const nextButton = document.getElementById('nextButton');
 var choiceList = document.getElementById('choiceList');
+var scores = document.getElementById('scores');
+var highScoresSection = document.getElementById('high-scores-section')
+var playerInitials = document.querySelector("#player-initials");
 var playAgainButton = document.querySelector("#play-again-button");
-var highScoresButton = document.getElementById("high-scores-button");
+var highScoresButton = document.getElementById('high-scores-button');
+var submitScoreButton = document.querySelector("#submit-score-button");
 
+var counter = 60;
 
 var questionArray = [
     {
-        question: 'What is 1+1?',
-        answer: '2',
-        choices: ['1', '2', '3']
+        question: 'Inside which HTML element do we put the JavaScript?',
+        answer: '<script>',
+        choices: ['<scripting>', '<script>', '<js>']
     },
     {
-        question: 'What is 3*3?',
-        answer: '9',
-        choices: ['10', '9', '8']
+        question: 'Where is the correct place to insert a JavaScript?',
+        answer: 'Both the <head> section and the <body> section are correct',
+        choices: ['The <head> section', 'Both the <head> section and the <body> section are correct', 'The <body> section']
     },
     {
 
-        question: 'What is 10*3?',
-        answer: '30',
-        choices: ['10', '20', '30']
+        question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+        answer: '<script src="xxx.js">',
+        choices: ['<script src="xxx.js">', '<script name="xxx.js">','<script href="xxx.js">']
+    },
+    {
+
+        question: 'The external JavaScript file must contain the <script> tag.',
+        answer: 'True',
+        choices: ['True', 'False','None of the above']
+    },
+    {
+
+        question: 'How do you create a function in JavaScript?',
+        answer: 'function myFunction()',
+        choices: ['function:myFunction()', 'function = myFunction()', 'function myFunction()']
     },
 ];
+var currentQuestion = 0;
 
 startBtn.addEventListener('click', function(e) {
     document.getElementById('home-screen').classList.add('hide');
@@ -39,12 +55,11 @@ function startGame() {
 }
 
 function startTimer(){
-    var counter = 60;
+    counter = 60;
     var interval = setInterval(function() {
-        // console.log(counter);
         counter--;
         timeEl.textContent = "Time Left: "+ counter
-        if (counter === 0 || counter < 0 ){
+        if (counter === 0 || counter < 0 || currentQuestion === 5 ){
             clearInterval(interval);
             endGame();
         }
@@ -52,70 +67,79 @@ function startTimer(){
 }
 
 function setQuestion(){
+    if( currentQuestion<5 ){
     document.getElementById('question').innerText = questionArray[currentQuestion].question;
     document.getElementById('choice-1').innerText = questionArray[currentQuestion].choices[0];
     document.getElementById('choice-2').innerText = questionArray[currentQuestion].choices[1];
-    document.getElementById('choice-3').innerText = questionArray[currentQuestion].choices[2];   
+    document.getElementById('choice-3').innerText = questionArray[currentQuestion].choices[2]; 
+    } 
+    else{
+        document.querySelector(".score-section").classList.remove('hide');
+        document.querySelector("#quiz-screen").classList.add('hide');
+    } 
 }
-
-var currentQuestion = 
-// question.textContent = currentQuestion.question;
-// choiceList.textContent = questionArray.choices;
 
 choiceList.addEventListener('click', function(event){
     console.log(event.target)
     console.log('you chose ' + event.target.innerText);
             var chosenAnswer = event.target.innerText
             if(chosenAnswer === questionArray[currentQuestion].answer) {
-                console.log('corrrect')
+                console.log('corrrect');
+                currentQuestion ++;
+                setQuestion();
             }
-            // increment currentQuestion. Call the setquestion function
+            else {
+                counter = counter - 10;
+            }
 })
 
-// 
-
-// backButton.addEventListener('click', goBack);
-// nextButton.addEventListener('click', goNext);
-
-// function showQuestion(index) {
-//     var questionArray  = currentQuestion[index];
-//     }
-
-// function goBack() {
-//     if (currentQuestion > 0) {
-//       currentQuestion--;
-//       showQuestion(currentQuestion);
-//     }
-//   }
-// function goNext() {
-//     if (currentQuestion < questionArray.length - 1) {
-//       currentQuestion++;
-//       showQuestion(currentQuestion);
-//     }
-//   }
- 
-
-//     function options(){
-//     var choices = document.getElementsByClassName('choice');
-//     console.log('choices', choices)
-//     for(var i = 0; i < choices.length; i++) {
-//         choices[i].addEventListener('click', function(e){
-//             console.log('you chose ' + e.target.innerText);
-//             var chosenAnswer = e.target.innerText
-//             if(chosenAnswer === questionArray[currentQuestion].answer) {
-//                 console.log('corrrect')
-//             }
-            
-//         })
-//     }
-// }
-
 function endGame(){
+    console.log('running endGame');
     startBtn.disabled = true;
-    clearInterval(interval)
+    document.querySelector(".high-score-section").classList.remove('hide');
+    quizScreen.classList.add('hide');
+    var pab = document.querySelector("#play-again-button");
+    pab.classList.remove('hide');
+    document.getElementById("high-scores-button").classList.remove('hide');
+    var navbar = document.querySelector("#navbar");
+    navbar.classList.remove('hide');
 }
 
-// write function for checking the answer
-// set up high score page
-//set up high score functionality
-//set up timer
+function saveScores(){
+    var oldScores = JSON.parse(localStorage.getItem("high-score"));
+    console.log(oldScores);
+    var allScoresArray = [];
+    var username = playerInitials.value;
+    if (playerInitials === ""){
+        alert("Type in your Initials")
+        return;
+    }
+    console.log(username);
+    var player = {initials: username, score: counter};
+    console.log(player);
+    // if theres no data in local storage
+    if(!oldScores){
+        // send current player highscore to local storage
+        allScoresArray.push(player);
+        localStorage.setItem("high-score", JSON.stringify(allScoresArray));
+    }
+    // if there IS data in local storage
+    if(oldScores){
+        // set scores in order
+        // add new scores
+        oldScores.push(player)
+        console.log(oldScores)
+        // discplay scores on screen
+        // reset oldScores in local storage
+    }
+}
+
+submitScoreButton.addEventListener('click', saveScores);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+// array push for oldscores + player
+// set up high score page with for loop for every item inside the highscores
+
